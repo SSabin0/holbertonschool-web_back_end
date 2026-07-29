9f2a307b2969cd2f3dbe65816997c876794c54db
SSabin0 <13078@holbertonstudents.com>
2026-07-29 12:23:20 +1000

    Fix indentation and import statement in sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Module to create sum of the annotated list """
-from typing import List
+from typing import
 
 
 def sum_list(input_list: List[float]) -> float:
-  """Return sum of annotated list as float"""
-  return sum(input_list)
+    """Return sum of annotated list as float"""
+    return sum(input_list)
