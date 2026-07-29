b7b17050784fb433089399750115569db0433090
SSabin0 <13078@holbertonstudents.com>
2026-07-29 12:19:36 +1000

    Change input_list type to List[float] for sum_list

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """ Module to create sum of the annotated list """
 
+from typing import List
 
-def sum_list(input_list: float) -> float:
+
+def sum_list(input_list: List[float]) -> float:
   """Return sum of annotated list as float"""
   return sum(input_list)
