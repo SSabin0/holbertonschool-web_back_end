90d3d85cdc2d81febcf77a6a86e7b71fb951947b
SSabin0 <13078@holbertonstudents.com>
2026-07-29 10:42:38 +1000

    Rename module and add concat function

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module to concat strings with typing """
+
+
+def concat(str1: str, str2: str) -> str:
+    """return the concat of two strings"""
+    return str1 + str2
+  
