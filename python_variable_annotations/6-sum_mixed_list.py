4bbf7ab02fdb2aa9726b53d18b864797314d9a17
SSabin0 <13078@holbertonstudents.com>
2026-07-29 12:43:34 +1000

    Create sum_mixed_list function for summing lists
    Fix the function signature to accept a list of mixed types.

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module to create sum of the annotated list """
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: Union[float]) -> float:
+    """Return sum of annotated list as float"""
+    return sum(mxd_lst)
