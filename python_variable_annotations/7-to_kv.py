2bee83e1c6a501ea7d4ea7aca9b80c85d00639f3
SSabin0 <13078@holbertonstudents.com>
2026-08-05 22:28:00 +1000

    Refactor to_kv function to always return float

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
+"""Module for to_kv function."""
 from typing import Tuple, Union
-"""Module to convert k into str and v into int or float """
+
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Return key-value pair with v squared if it's a float."""
-    if isinstance(v, float):
-        return k, v ** 2
-    return k, v
+    """Return a tuple of the string k and the square of v as a float."""
+    return (k, float(v ** 2))
