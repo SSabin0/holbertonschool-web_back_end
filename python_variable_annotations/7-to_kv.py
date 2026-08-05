ef3fa3bcd5c5fb912d948ebebdd4bf18ad3c109c
SSabin0 <13078@holbertonstudents.com>
2026-08-05 22:25:39 +1000

    Clean up print statements in to_kv.py
    Removed print statements for function annotations and calls.

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,7 +6,3 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     if isinstance(v, float):
         return k, v ** 2
     return k, v
-
-print(to_kv.__annotations__)
-print(to_kv("eggs", 9))
-print(to_kv("school", 0.02))
