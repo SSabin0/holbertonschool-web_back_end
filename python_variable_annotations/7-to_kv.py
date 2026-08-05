e3e8725366d03de03af311b5ef88cbed75276401
SSabin0 <13078@holbertonstudents.com>
2026-08-05 22:26:38 +1000

    Enhance to_kv function with docstring and type hint
    Add module docstring and update function annotation.

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from typing import Tuple, Union
+"""Module to convert k into str and v into int or float """
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return key-value pair with v squared if it's a float."""
