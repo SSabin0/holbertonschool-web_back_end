403aa36ca0b3b8d5465c9743754f4388882b9007
SSabin0 <13078@holbertonstudents.com>
2026-08-10 22:53:29 +1000

    Delete ython_async_function directory

--- ython_async_function/0-basic_async_syntax.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import random
-"""Create a module to generate a random wait time based on 0 to max delay number given"""
-
-async def wait_random(max_delay: int = 10) -> float:
-  """Wait for a random amount of time and return the delay"""
-    random_number = random.uniform(0, max_delay)
-    await asyncio.sleep(random_number)
-    return random_number
