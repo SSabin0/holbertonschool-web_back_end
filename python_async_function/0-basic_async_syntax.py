b5aeb7878315d2fac123bad123aa737047be3b0b
SSabin0 <13078@holbertonstudents.com>
2026-08-10 22:54:04 +1000

    Add docstring to wait_random function
    Added a docstring for the wait_random function to explain its purpose.

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -2,6 +2,7 @@ import asyncio
 import random
 """Create a module to generate a random wait time based on 0 to max delay number given"""
 
+
 async def wait_random(max_delay: int = 10) -> float:
   """Wait for a random amount of time and return the delay"""
     random_number = random.uniform(0, max_delay)
