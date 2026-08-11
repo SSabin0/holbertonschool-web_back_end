da0b06bbe5a2a9fe282afc224ad86c818d57d2a1
SSabin0 <13078@holbertonstudents.com>
2026-08-11 14:39:04 +1000

    Fix indentation and add return statement in wait_random

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -2,7 +2,6 @@
 """This module provides an async coroutine that waits
 for a random delay before returning it.
 """
-
 import asyncio
 import random
 
@@ -11,5 +10,7 @@ async def wait_random(max_delay: int = 10) -> float:
   """Wait for a random amount of time and return the delay"""
   
     random_number = random.uniform(0, max_delay)
+
     await asyncio.sleep(random_number)
+
     return random_number
