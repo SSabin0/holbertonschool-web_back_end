b3c7b88f1c4f001c3c6e11d0bf26ca3b39e04058
SSabin0 <13078@holbertonstudents.com>
2026-08-11 13:52:30 +0200

    add async gen

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,9 @@ import random
 
 
 async def async_generator():
-    """Yield 10 random numbers asynchronously."""
+    """Module to asynchronously wait 1 second then yield a number between
+    0 and 10.
+    """
     for i in range(10):
         await asyncio.sleep(1)
         number = random.uniform(0, 10)
