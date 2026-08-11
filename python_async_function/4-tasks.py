85db8c49b82697336ab02de19eb63e07b8ffd45b
SSabin0 <13078@holbertonstudents.com>
2026-08-11 13:37:22 +0200

    add task 2

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ task_wait_random = __import__("3-tasks").task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn task_wait_random n times with max_delay and return delays in ascending order."""
+    """Spawn task_wait_random n times then return delays in asc order."""
     coroutines = []
     for i in range(n):
         coroutines.append(task_wait_random(max_delay))
