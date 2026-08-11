3e6f1d26744c74e3c7316953a39af46f69c7427a
SSabin0 <13078@holbertonstudents.com>
2026-08-11 12:47:31 +0200

    add concurrent croutines

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module for multiple coroutines at the same time"""
+
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    coroutines = []
+    for i in range(n):
+        coroutines.append(wait_random(max_delay))
+    
+    results = []
+    for coroutine in asyncio.as_completed(coroutines):
+        result = await coroutine
+        results.append(result)
+    
+    return results
