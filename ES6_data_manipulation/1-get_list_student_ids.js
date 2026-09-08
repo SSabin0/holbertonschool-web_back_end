4d8a9c9b5d278e4685c3fd61e1d926a2df9f6721
SSabin0 <13078@holbertonstudents.com>
2026-09-08 18:03:15 +1000

    Create 1-get_list_student_ids.js

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(students) {
+  if (Array.isArray(students)) {
+    return students.map(students => students.id);
+  }
+    else {
+      return [];
+    }
+}
