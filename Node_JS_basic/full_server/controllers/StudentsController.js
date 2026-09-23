2ab53c2267c7860cbddf10ff66a93c436783c324
SSabin0 <13078@holbertonstudents.com>
2026-09-23 22:59:16 +1000

    Implement StudentsController with student retrieval methods

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,1 +1,44 @@
+import readDatabase from '../utils';
 
+class StudentsController {
+  static getAllStudents(request, response) {
+    const databasePath = process.argv[2];
+
+    readDatabase(databasePath)
+      .then((fields) => {
+        let output = 'This is the list of our students\n';
+        const sortedFields = Object.keys(fields).sort((a, b) => a.localeCompare(b, 'en', { sensitivity: 'base' }));
+
+        sortedFields.forEach((field) => {
+          const names = fields[field];
+          output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+        });
+
+        response.status(200).send(output.trim());
+      })
+      .catch(() => {
+        response.status(500).send('Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    const databasePath = process.argv[2];
+    const { major } = request.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      response.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    readDatabase(databasePath)
+      .then((fields) => {
+        const names = fields[major] || [];
+        response.status(200).send(`List: ${names.join(', ')}`);
+      })
+      .catch(() => {
+        response.status(500).send('Cannot load the database');
+      });
+  }
+}
+
+export default StudentsController;
