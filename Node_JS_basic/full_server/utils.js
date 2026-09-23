30650c7e0b6639a957fed2daa8cc385ac30ad9de
SSabin0 <13078@holbertonstudents.com>
2026-09-23 22:58:46 +1000

    Add readDatabase function to read data from file

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,1 +1,26 @@
+import fs from 'fs';
 
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const fields = {};
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const [, ...rows] = lines; // skip the header row
+
+    rows.forEach((row) => {
+      const [firstname, , , field] = row.split(',');
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    });
+
+    resolve(fields);
+  });
+});
+
+export default readDatabase;
